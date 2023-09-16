d3f2d5128a40a0e01e90cfb889fb54099ad65cbf
riosol7 <piraiojeda@gmail.com>
2023-09-15 18:03:24 -0700

    getTotalPts updated

--- dynasty/src/utils/stats/getTotalPts.ts
+++ dynasty/src/utils/stats/getTotalPts.ts
@@ -1,65 +1,25 @@
 import { roundToHundredth }from "./calculationUtils";
 import * as Interfaces from "../../interfaces";
 
-// matches: Interfaces.Match[][]
-
 export const getTotalPts = (legacyLeague: Interfaces.League[], rID: number, pID: string) : { pts: number, maxPts: number } => {
+    
+    const pts = legacyLeague.map((league: Interfaces.League) => 
+        league.matchups.map(week => week.find(team => team.roster_id === rID)?.starters.find((starter: string) => starter === pID) !== undefined ? 
+            Object.entries(week.filter(team => team.roster_id === rID)[0].players_points).filter(player => player[0] === pID)[0][1] : 0
+        ).reduce((partialSum: number , a: any) => partialSum + a, 0)
+    ).reduce((partialSum: number , a: any) => partialSum + a, 0);
 
-    const pts = legacyLeague.map((league: Interfaces.League) => league.matchups.map(week => week.filter(team => team.roster_id === rID)[0]?.starters.find((starter: string) => starter === pID) !== undefined ? 
-        Object.entries(week.filter(team => team.roster_id === rID)[0].players_points).filter(player => player[0] === pID)[0][1] : 0
-    ).reduce((partialSum: number , a: any) => partialSum + a, 0));
-    
-    const maxPts = 0;
-    
-    console.log("pts", pts)
-    
+    const maxPts = legacyLeague.map((league: Interfaces.League) => 
+        league.matchups.map(week => Object.entries(week.find(team => team.roster_id === rID).players_points).find(player => player[0] === pID)
+    )).flat().reduce((total, playerData: any) => {
+        if (playerData) {
+        total += playerData[1];
+        }
+        return total;
+    }, 0);
+        
     return {
-        pts: roundToHundredth(pts[0]),
+        pts: roundToHundredth(pts),
         maxPts: roundToHundredth(maxPts)
     };
 };
-
-
-
-// let historyMaxPts: number = 0;
-//     let currentMaxPts: number = 0;
-//     let historyPts: number = 0; 
-//     let currentPts: number = 0;
-
-//     historyPts = league?.history?.map(legacy => 
-//         Object.entries(legacy.matchups).map((week: any) => week[1].filter((team: Interfaces.Match) => team.roster_id === rID)[0].starters
-//             .find((starter: string) => starter === pID) !== undefined 
-//             ? Object.entries(week[1].filter((team: Interfaces.Match) => team.roster_id === rID)[0].players_points)
-//                 .filter(p => p[0] === pID)[0][1] 
-//             : 0
-//         ).reduce((partialSum: number, a: any) => partialSum + a, 0)
-//     ).reduce((partialSum: number, a: any) => partialSum + a, 0);
-
-//     currentPts = matches?.map(match => match.filter(team => team.roster_id === rID)[0]?.starters.find((starter: string) => starter === pID) !== undefined 
-//         ? Object.entries(match.filter(team => team.roster_id === rID)[0].players_points)
-//             .filter(player => player[0] === pID)[0][1]
-//         : 0
-//         ).reduce((partialSum,a) => partialSum + a, 0)
-
-//     let checkHUndefined = league?.history?.map(legacy => Object.entries(legacy.matchups)
-//         .map((week: any) => Object.entries(week[1].filter((team: Interfaces.Match) => team.roster_id === rID)[0].players_points)
-//             .filter(player => player[0] === pID)[0]).filter(team => team !== undefined)).filter(team => team.length > 0);
-    
-//     let checkCUndefined: any[] = [];
-
-//     if (matches[0]?.length > 0) {
-//         checkCUndefined = matches?.map(week => Object.entries(week.filter(team => team.roster_id === rID)[0]?.players_points)
-//             .filter(player => player[0] === pID)[0]).filter(team => team !== undefined)
-//             // .filter(team => team !== [])
-//     }
-
-//     if (checkHUndefined && checkHUndefined[0]?.length > 0 && checkCUndefined?.length > 0) {
-//         historyMaxPts = checkHUndefined.map(l => l.map(a => a[1]).reduce((partialSum: number , a: any) => partialSum + a, 0)).reduce((partialSum, a) => partialSum + a, 0)
-//         currentMaxPts = checkCUndefined.map((a) => a[1]).reduce((partialSum,a) => partialSum + a, 0)
-
-//     } else if(checkHUndefined && checkHUndefined[0]?.length > 0 && checkCUndefined?.length === 0){
-//         historyMaxPts = checkHUndefined.map(l => l.map(a => a[1]).reduce((partialSum: number, a: any) => partialSum + a, 0)).reduce((partialSum, a) => partialSum + a, 0)
-
-//     } else if(checkCUndefined?.length > 0){
-//         currentMaxPts = checkCUndefined.map((a) => a[1]).reduce((partialSum,a) => partialSum + a, 0)
-//     }
