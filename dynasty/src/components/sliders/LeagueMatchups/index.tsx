d51c0ec36a7e20315de38a34d255fecf07e3cd9b
riosol7 <piraiojeda@gmail.com>
2023-10-25 01:16:14 -0700

    LeagueMatchup Slider init

--- dynasty/src/components/sliders/LeagueMatchups/index.tsx
+++ dynasty/src/components/sliders/LeagueMatchups/index.tsx
@@ -19,28 +19,44 @@ export default function LeagueMatchupSlider({ selectWeek }: {selectWeek: number}
 
     return (
         <div className={styles.matchupSlide}>
-            {selectedMatchups.map((matchup: Interfaces.Match[], i: number) => 
+            {selectedMatchups?.map((matchup: Interfaces.Match[], i: number) => 
                 <div key={i} className={styles.matchupCard}>
                     {matchup.map((team, idx) => {
                         const roster = findRosterByRosterID(team.roster_id, league.rosters);
                         const starterPts = team?.starters_points?.sort((a,b) => b - a);
                         const topStarter = findPlayerByPts(team, starterPts[0]!, players);
+                        const flexDirection = idx === 0;
                         return (
-                            <div key={idx} className={styles.teamCard} style={{flexDirection: idx === 0 ? "row" : "row-reverse" }}>
+                            <div key={idx} className={`${styles.teamCard}`} style={{flexDirection: flexDirection ? "row" : "row-reverse", textAlign: flexDirection ? "start" : "end" }}>
                                 <div className={styles.playerBackground} style={(players.length > 0) ? {background:findLogo(topStarter.team || "FA").bgColor}:{}}>
                                     <div className={styles.teamLogo} style={{ backgroundImage:`url(${findLogo(topStarter.team).l})`, backgroundPosition: topStarter.position === "DEF" ? "center" : "top"}}>
                                         <div className={styles.player} style={{ backgroundImage: `url(${PLAYER_BASE_URL}${topStarter.player_id}.jpg)` }}></div>
                                     </div>
                                 </div>
-                                <div className={styles.teamInfo}>
-                                    <div className="flex items center">
+                                <div className={`${styles.teamInfo}`}>
+                                    {flexDirection ?
+                                    <div className={`flex items-center justify-start`}>
                                         <div className={styles.teamAvatar} style={{ backgroundImage: `url(${SLEEPER_AVATAR_BASE_URL}${roster?.owner?.avatar})`}}></div>
-                                        <div className="ml-2">
+                                        <div className={"ml-2"}>
                                             <p className="font-bold">{roster.owner.display_name}</p>
-                                            <p className="text-xs">{findRecord(team.roster_id, selectedMatchups, selectWeek).record}</p>
+                                            <div className="text-xs flex items-center justify-between">
+                                                <p>{findRecord(team.roster_id, selectedMatchups, selectWeek).record}</p> 
+                                                <p>{team.points}</p>
+                                            </div>
                                         </div>
                                     </div>
-                                    <p>{team.points}</p>
+                                    :
+                                    <div className={`flex items-center justify-end`}>
+                                        <div className={"pr-2"}>
+                                            <p className="font-bold">{roster.owner.display_name}</p>
+                                            <div className="text-xs flex items-center justify-between">
+                                                <p>{team.points}</p>
+                                                <p>{findRecord(team.roster_id, selectedMatchups, selectWeek).record}</p> 
+                                            </div>
+                                        </div>
+                                        <div className={styles.teamAvatar} style={{ backgroundImage: `url(${SLEEPER_AVATAR_BASE_URL}${roster?.owner?.avatar})`}}></div>
+                                    </div>
+                                    }
                                     <p>{topStarter.first_name} {topStarter.last_name}</p>
                                 </div>
                             </div>
